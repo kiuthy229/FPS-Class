94f3af0cfd89ad4ef9f71e25e346d892db9ad7ea
kieuthy <thynmkgcs190847@fpt.edu.vn>
2022-08-17 22:34:48 +0700

    shooter

---
+++ Assets/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Camera fpsCam;
+
+    public float range =180f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            ShootRay();
+        }
+
+        void ShootRay(){
+            RaycastHit hit;
+
+            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+                Debug.Log(hit.transform.name);
+            }
+            else{
+                Debug.Log("Missed");
+            }
+        }
+    }
+}
---
+++ Assets/Walk.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public CharacterController CC;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpForce = 3f;
+    public float checkRadius = 3f;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
+    bool isGrounded;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, checkRadius, whatIsGround);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        float X = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward*z;
+
+        CC.Move(move * speed *Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpForce *gravity* -2f);
+        }
+
+        velocity.y +=gravity*Time.deltaTime;
+
+        CC.Move(velocity * Time.deltaTime);
+    }
+}
